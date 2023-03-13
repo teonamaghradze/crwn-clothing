c5ac603dfd4493c362bcaa2badafa4f02b729c38
teonamaghradze <maghradzeteo@gmail.com>
2023-03-13 13:16:44 +0400

    Add crwn clothing project

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,16 @@
+import "./directory.styles.scss";
+import CategoryItem from "../category-item/category-item.component";
+
+const Directory = ({ categories }) => {
+  return (
+    <div>
+      <div className="directory-container">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Directory;
